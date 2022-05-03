47f9240eed79b38ca677bca49cc608f31f229053
Carl Smestad <carl.smestad@gmail.com>
2022-05-03 18:55:25 +0200

    finish

--- studio/deskStructure.ts
+++ studio/deskStructure.ts
@@ -1,4 +1,4 @@
-import { MdAccountCircle, MdChatBubble } from "react-icons/md";
+import { MdAccountCircle, MdChatBubble, MdGroup } from "react-icons/md";
 
 import Iframe from "sanity-plugin-iframe-pane";
 import S from "@sanity/desk-tool/structure-builder";
@@ -6,6 +6,7 @@ import resolveProductionUrl from "./resolveProductionUrl";
 
 // We filter document types defined in structure to prevent
 // them from being listed twice
+const hiddenDocTypes = (listItem) => !["post", "author"].includes(listItem.getId());
 
 export const getDefaultDocumentNode = () => {
     // Return all documents with just 1 view: the form
@@ -25,4 +26,30 @@ export default () =>
     S.list()
         .title("Site")
         .items([
+            S.listItem()
+                .title("Posts")
+                .icon(MdChatBubble)
+                .schemaType("post")
+                .child(S.documentTypeList("post").title("Posts")),
+            S.listItem()
+                .title("Authors")
+                .icon(MdAccountCircle)
+                .schemaType("author")
+                .child(S.documentTypeList("author").title("Authors")),
+            S.listItem()
+                .title("Posts by author")
+                .icon(MdGroup)
+                .child(
+                  S.documentTypeList("author")
+                    .title("Posts by author")
+                    .child(
+                      (_id) =>
+                        S.documentTypeList("post")
+                          .schemaType("post")
+                          .title("Posts by author")
+                          .filter('_type == "post" && author._ref == $_id')
+                          .params({ _id })
+                       )
+                   ),
+            ...S.documentTypeListItems().filter(hiddenDocTypes),
         ]);
--- studio/schemas/schema.ts
+++ studio/schemas/schema.ts
@@ -2,6 +2,9 @@
 import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
+import author from "./documents/author";
+import post from "./documents/post";
+import deskStructure from "../deskStructure";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -10,5 +13,7 @@ export default createSchema({
     // Then proceed to concatenate our document type
     // to the ones provided by any plugins that are installed
     types: schemaTypes.concat([
+        author,
+        post,
     ]),
 });
